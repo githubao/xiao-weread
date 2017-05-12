33be5debf94b2275d0bc51b1d299769d72b5e383
BaoQiang <mailbaoqiang@gmail.com>
2017-05-12 21:47:36 +0800

    final run

--- xiaoreader/app.py
+++ xiaoreader/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, make_response, request, abort
 
 from xiaoreader.db.mymongo import MyMongo
+import time
 
 # 静态文件的放置路径，可根据实际情况设置，这里设置为默认路径：'./static/'
 app = Flask(__name__, static_url_path='')
@@ -25,8 +26,38 @@ def root():
     return app.send_static_file('index.html')
 
 
+@app.route('/item/<bookid>', methods=['GET'])
+def item(bookid):
+    return app.send_static_file('item.html')
+
+
+# @app.route('/item/<bookid>', methods=['GET'])
+# def book(bookid):
+#     result = mongo.get_one('xiao', 'wxread', 'id', bookid)
+#     return app.make_response((json.dumps(result), 200))
+
+
+
+@app.route('/api/item', methods=['POST'])
+def get_item():
+    __order__ = ['id', 'url', 'title', 'cate', 'intro', 'vote_cnt', 'star']
+
+    if request.json['bid'] == "":
+        abort(400)
+
+    bid = request.json['bid']
+
+    result = mongo.get_one('xiao', 'wxread', 'id', bid)
+
+    order_result = []
+    for name in __order__:
+        order_result.append({'key': name, 'value': result.get(name)})
+
+    return jsonify({'result': order_result}), 200
+
+
 # POST方法API，添加数据项
-@app.route('/search', methods=['POST'])
+@app.route('/api/search', methods=['POST'])
 def search():
     if request.json['query'] == "":
         abort(400)
@@ -41,9 +72,15 @@ def search():
         result_list.sort(key=lambda x: x['vote_cnt'], reverse=True)
 
     for idx, item in enumerate(result_list):
-        item.update({'id': idx + 1})
+        dic = {'seqid': idx + 1, 'date': get_current(), 'url': '/item/{}'.format(item['id']),
+               'short': '{}...'.format(item['intro'][:110]) if len(item['intro']) >= 110 else item['intro']}
+        item.update(dic)
         results.append(item)
-    return jsonify({'results': results}), 201
+    return jsonify({'results': results}), 200
+
+
+def get_current():
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
 
 
 # 404
